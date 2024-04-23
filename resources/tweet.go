74b393411621525709c82502d81941e3746eafcb
miyaviee <merveilles19980318@gmail.com>
2024-04-23 12:17:55 +0900

    PublicMetricsにImpressionCountを追加 (#8)
    BUG: qlonolink/qua#19068

--- resources/tweet.go
+++ resources/tweet.go
@@ -117,10 +117,12 @@ type PromotedMetrics struct {
 }
 
 type TweetPublicMetrics struct {
-	RetweetCount *int `json:"retweet_count"`
-	ReplyCount   *int `json:"reply_count"`
-	LikeCount    *int `json:"like_count"`
-	QuoteCount   *int `json:"quote_count"`
+	RetweetCount    *int `json:"retweet_count"`
+	ReplyCount      *int `json:"reply_count"`
+	LikeCount       *int `json:"like_count"`
+	QuoteCount      *int `json:"quote_count"`
+	ImpressionCount *int `json:"impression_count"`
+	BookmarkCount   *int `json:"bookmark_count"`
 }
 
 type ReferencedTweet struct {
